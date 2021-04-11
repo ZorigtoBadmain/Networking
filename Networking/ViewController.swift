576dfc52308c19221b67924dd5ace8d550c31ddc
Zorigto Badmain <badmainzzz@mail.ru>
2021-04-11 15:45:56 +0900

    new progect GET and POST

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -9,37 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var getImageButton: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        activityIndicator.isHidden = true
-        activityIndicator.hidesWhenStopped = true
+    @IBAction func getRequest(_ sender: Any) {
     }
-
-    @IBAction func getImagePressed(_ sender: Any) {
-        
-        label.isHidden = true
-        getImageButton.isEnabled = false
-        activityIndicator.isHidden = false
-        activityIndicator.startAnimating()
-        
-        guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg") else { return }
-        
-        let session = URLSession.shared
-        session.dataTask(with: url) { (data, response, error) in
-            if let data = data, let image = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    self.activityIndicator.stopAnimating()
-                    self.imageView.image = image
-                }
-            }
-        }.resume()
+    
+    @IBAction func postRequest(_ sender: Any) {
     }
     
 }
